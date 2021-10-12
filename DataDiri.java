80588b3fa0535fe2fc1a55d78941ace93f0155c7
Elroi <elroi4677@gmail.com>
2021-10-12 20:59:26 +0700

    java-code-challenge-1

---
+++ DataDiri.java
@@ -0,0 +1,26 @@
+public class DataDiri{
+   public static void main(String[] args){
+       String firstName, lastName, alamat, hobi, makananKesukaan, phoneNumber;
+        int usia;
+        double tinggi, beratBadan;
+
+        firstName = "Elroi ";
+        lastName = "Purba";
+        alamat = "Perumahan Mutiara Hijau Blok B2 No.5";
+        hobi = "Bermain Musik";
+        phoneNumber = "089623503910";
+        usia = 20;
+        tinggi = 165.5;
+        beratBadan = 55;
+        makananKesukaan = "Mie Nasi Goreng";
+
+        System.out.println("Nama : " + firstName + lastName);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Usia : " + usia + " tahun");
+        System.out.println("Tinggi Badan : " + tinggi + " cm");
+        System.out.println("Berat Badan : " + beratBadan + " kg");
+        System.out.println("Hobi : " + hobi);
+        System.out.println("Nomor Telepon : " + phoneNumber);
+        System.out.println("Makanan Kesukaan : " + makananKesukaan);
+   } 
+}
